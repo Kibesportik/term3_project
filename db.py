bcad55307c2348d33b81df90c9a381f5376e24dd
Aleks Svietik <alexeysvietik@gmail.com>
2024-11-21 17:19:01 +0000

    lesson_commit

--- db.py
+++ db.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime, Foreign
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
 
+
+
 DATABASE_URI = 'sqlite:///app.db'
 
 engine = create_engine(DATABASE_URI)
