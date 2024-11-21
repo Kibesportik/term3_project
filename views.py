8a2972037ca279c621251ca28db978ee71e6fe99
Aleks Svietik <alexeysvietik@gmail.com>
2024-11-21 07:52:03 +0000

    base-index_extention

--- views.py
+++ views.py
@@ -4,9 +4,12 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi import Request, Depends, Form
 from sqlalchemy.orm import Session
 
+def login_user(username,db: Session = Depends(get_db)):
+    user = db.query(User).filter_by(username=username).first()
+    if user is None:
+        return RedirectResponse(url='/login', status_code=302)
+
 @app.get('/',response_class=HTMLResponse)
 def index(request:Request, db: Session = Depends(get_db), username: str = Form()):
-    user = db.query(User).filter_by(username = username).first()
-    if user is None:
-        return RedirectResponse(url='/login',status_code = 302)
+    login_user(username)
     return templates.TemplateResponse('index.html',{'request': request})
